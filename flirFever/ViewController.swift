dcc099d03a99f9676d2907c639973e526aad32a1
Lee Ming Tsun <dg6546@gmail.com>
2021-03-02 18:25:26 +0800

    v3

--- flirFever/ViewController.swift
+++ flirFever/ViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataReceivedDelegate {
     let camera = FLIRCamera()
     let discovery = FLIRDiscovery()
     var flip:Int = 0
+    
     @IBOutlet weak var cameraView: UIImageView!
     
     @IBOutlet weak var readingLabel: UILabel!
@@ -84,7 +86,31 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
                 }else{
                     showImage = image.getImage()
                 }
+                
+                let request = VNDetectFaceRectanglesRequest { [unowned self] request, error in
+                    if let error = error {
+                        
+                    }
+                    else {
+                        self.handleFaces(with: request)
+                    }
+                }
+                let handler:VNImageRequestHandler
+                if self.flip==0{
+                    handler = VNImageRequestHandler(cgImage: showImage.cgImage!, options:[:] )
+                }
+                else{
+                    handler = VNImageRequestHandler(cgImage: showImage.cgImage!, orientation:.upMirrored ,options:[:] )
+                }
+                do {
+                    try handler.perform([request])
+                }
+                catch {
+                    
+                }
                 self.cameraView.image = showImage
+                
+                /* //test point measurement
                 if image.measurements.measurementSpots.count == 0{
                     image.measurements.addSpot(CGPoint(x: Int(image.getWidth())/2, y: Int(image.getHeight())/2))
                 }
@@ -92,41 +118,50 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
                 //self.readingLabel.text = spot?.value.value() as? String
                 //print(spot?.value.value ?? "nil")
                 self.readingLabel.text = "\(String(format: "%.1f", (spot?.value.value)!-273.15+2 ))"
+                */
             }
+            
         }
     }
+             
     
-    
-/*    func imageReceived() {
-        DispatchQueue.global(qos: .default).async(execute: { [self] in
-            do{
-                self.camera.withImage({ (image:FLIRThermalImage!) in
-                let fusion:FLIRFusion! = image.getFusion()
-                    fusion.setFusionMode(VISUAL_MODE)
-                if self.flip==1 {
-                    let sourceImage:UIImage! = image.getImage()
-                    let flippedImage:UIImage! = UIImage(cgImage: sourceImage.cgImage!,
-                                                            scale:sourceImage.scale,
-                                                            orientation: UIImage.Orientation.upMirrored)
-                    self.cameraView.image = flippedImage
-                }else{
-                    self.cameraView.image = image.getImage()
-                }
+    func handleFaces(with request: VNRequest) {
+        cameraView.layer.sublayers?.forEach { layer in
+            layer.removeFromSuperlayer()
+        }
+        guard let observations = request.results as? [VNFaceObservation] else {
+            return
+        }
+        observations.forEach { observation in
+            let boundingBox = observation.boundingBox
+            let origin:CGPoint
+            let size:CGSize
 
-                    if image.measurements.measurementSpots.count==0{
-                        image.measurements.addSpot(CGPoint.init(x: Int(image.getWidth())/2, y: Int(image.getHeight())/2))
-                    }
-
-                    let spot:FLIRMeasurementSpot! = image.measurements.measurementSpots.firstObject as? FLIRMeasurementSpot
-                    self.readingLabel.text = String(format:"%.1f",spot.value)
-            })
-            }
-        })
+            size = CGSize(width: boundingBox.width * cameraView.bounds.width,
+                                  height: boundingBox.height * cameraView.bounds.height)
+            origin = CGPoint(x: boundingBox.minX * cameraView.bounds.width,
+                                     y: (1 - observation.boundingBox.minY) * cameraView.bounds.height - size.height)
             
+            
+            
+            
+            let layer = CAShapeLayer()
+            layer.frame = CGRect(origin: origin, size: size)
+            layer.borderColor = UIColor.red.cgColor
+            layer.borderWidth = 2
+            
+            cameraView.layer.addSublayer(layer)
+        }
     }
-*/
-    
+                
+                
+                
+                
+                
 
+                
+             
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
