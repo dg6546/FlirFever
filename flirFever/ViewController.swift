2b1aa6f27f7b99be7a04c7c9d5e1597ba1b44d18
Lee Ming Tsun <dg6546@gmail.com>
2021-03-14 11:00:32 +0800

    kill me

--- flirFever/ViewController.swift
+++ flirFever/ViewController.swift
@@ -7,17 +7,23 @@
 
 import UIKit
 import Vision
+import Toast
 
-class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataReceivedDelegate {
+class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataReceivedDelegate, FLIRRemoteDelegate {
     let camera = FLIRCamera()
     let discovery = FLIRDiscovery()
+    var calibration = FLIRCalibration()
+    var battery = FLIRBattery()
+    let pm = FLIRPaletteManager()
     var flip:Int = 0
+    
+    @IBOutlet weak var chargingLabel: UILabel!
+    
+    @IBOutlet weak var batteryLabel: UILabel!
     
     @IBOutlet weak var cameraView: UIImageView!
     
-    @IBOutlet weak var readingLabel: UILabel!
     
-    @IBOutlet weak var statusText: UITextView!
     func cameraFound(_ cameraIdentity: FLIRIdentity) {
         if cameraIdentity.cameraType() == FLIRCameraType.flirOne{
             
@@ -26,41 +32,66 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
                     try camera.connect(cameraIdentity)
                     
                 }catch{
-                    fatalError("Unable to connect camera")
+                    self.view.makeToast("Unable to connect camera", duration: 3.0, position: .bottom)
+                    return
                 }
                 DispatchQueue.main.async(execute: { [self] in
-                    readingLabel.isHidden = false
                     discovery.stop()
+                    
+                    //macbook(the sdk...)debug delegate method
+                    //it is better if these delegate can work on iphone
+                    self.camera.getRemoteControl()?.delegate = self
+                    self.camera.getRemoteControl()?.getBattery()?.subscribePercentage()
+                    self.camera.getRemoteControl()?.getBattery()?.subscribeChargingState()
+                    self.camera.getRemoteControl()?.getCalibration()?.subscribeCalibrationState()
+                    
+                    
+                    calibration = (camera.getRemoteControl()?.getCalibration())!
+                    battery = (camera.getRemoteControl()?.getBattery())!
+                    getBattery()
                         do{
                             try camera.subscribeStream()
-                            
                         }catch{
-                            fatalError("Unable to subscribe to stream")
+                            self.view.makeToast("Unable to subscribe to stream", duration: 3.0, position: .bottom)
+                            return
                         }
                     })
                 })
-//            do{
-//                try camera.connect(cameraIdentity)
-//                statusText.text.append("Camera connected\n")
-//                self.discovery.stop()
-//                try self.camera.subscribeStream()
-//                statusText.text.append("Camera subscribed\n")
-//            }catch let error as NSError {
-//                statusText.text.append("\(error)")
-//            }
+            calibrate()
+            self.view.makeToast("Camera connected", duration: 3.0, position: .bottom)
         }
+
+        
     }
     
+    func calibrationStateChanged(_ state: Bool) {
+        //trash, does not work
+    }
+    
+    func chargingStateChanged(_ state: FLIRChargingState) {
+        //trash, does not working
+    }
+    
+    func percentageChanged(_ percent: Int32) {
+        //trash, does not working
+    }
+    
+    
     func discoveryError(_ error: String, netServiceError nsnetserviceserror: Int32, on iface: FLIRCommunicationInterface) {
-        NSLog("Error %s", error)
+        self.view.makeToast("Cannot find camera.", duration: 3.0, position: .bottom)
     }
     
     func onDisconnected(_ camera: FLIRCamera, withError error: Error?) {
         camera.unsubscribeStream()
+        self.camera.getRemoteControl()?.getCalibration()?.unsubscribeCalibrationState()
+        self.camera.getRemoteControl()?.getBattery()?.unsubscribePercentage()
+        self.camera.getRemoteControl()?.getBattery()?.unsubscribeChargingState()
+        self.view.makeToast("Camera disconnected", duration: 3.0, position: .bottom)
     }
     
     func cameraLost(_ cameraIdentity: FLIRIdentity) {
         camera.disconnect()
+        self.view.makeToast("Camera disconnected", duration: 3.0, position: .bottom)
     }
     
     @IBAction func Flip(_ sender: Any) {
@@ -80,10 +111,18 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
 //                layer.removeFromSuperview()
 //            }
             self.camera.withImage { (image: FLIRThermalImage) in
-                let showImage:UIImage!
-                let sourceImage:UIImage! = image.getImage()
                 let fusion:FLIRFusion! = image.getFusion()
-                    fusion.setFusionMode(VISUAL_MODE)
+                image.palette = self.pm.iron
+                //image.rotate(RotationAngle(rawValue: 0)!)
+                fusion.setFusionMode(IR_MODE)
+                let irImage:UIImage! = image.getImage()
+                fusion.setFusionMode(VISUAL_MODE)
+                let colorImage:UIImage! = image.getImage()
+                image.setTemperatureUnit(TemperatureUnit(rawValue: 0)!)
+                
+                    //fusion.setFusionMode(VISUAL_MODE)
+                
+                
                 var bBoxList = [CGRect]()
 //                if self.flip == 1{
 //                    let flippedImage:UIImage! = UIImage(cgImage: sourceImage.cgImage!,scale:sourceImage.scale,
@@ -92,7 +131,6 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
 //                }else{
 //                    showImage = image.getImage()
 //                }
-                showImage = image.getImage()
                 let request = VNDetectFaceRectanglesRequest { [unowned self] request, error in
                     if error != nil {
                         
@@ -102,7 +140,8 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
                     }
                 }
                 let handler:VNImageRequestHandler
-                handler = VNImageRequestHandler(cgImage: showImage.cgImage!, options:[:] )
+                //handler = VNImageRequestHandler(cgImage: colorImage.cgImage!, options:[:] )
+                handler = VNImageRequestHandler(cgImage: colorImage.cgImage!, options:[:] )
 //                if self.flip==0{
 //                    handler = VNImageRequestHandler(cgImage: showImage.cgImage!, options:[:] )
 //                }
@@ -120,19 +159,31 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
                         let x = bBox.midX
                         let y = bBox.midY
                         let point = CGPoint(x: x,y: y)
-                        
+                        let bBoxCenterPoint = CGPoint(x: bBox.size.width/2 + bBox.origin.x, y: bBox.origin.y) 
                         image.measurements.addCircle(point, radius: Int32(Int(y-bBox.minY)))
-                        let label = UILabel(frame: (CGRect(origin: bBox.origin, size: CGSize(width:50,height:20))))
+                        let label = UILabel(frame: (CGRect(origin: bBoxCenterPoint, size: CGSize(width:50,height:20))))
                         let spot  = image.measurements.measurementCircles.firstObject as? FLIRMeasurementCircle
-                        label.text = "\(String(format: "%.1f", (spot?.max.value)!-273.15+2 ))"
+                        guard let reading = spot?.max.value else{
+                            return
+                        }
+                        
+                        
+                        
+                        //reading = reading  - 273
+                        if reading > 37.5{
+                            label.textColor = UIColor.red
+                        }else{
+                            label.textColor = UIColor.green
+                        }
+                        label.text = "\(String(format: "%.1f", (reading) ))"
                         self.cameraView.addSubview(label)
                     }
 
                 }
                                 
                 
-                
-                self.cameraView.image = showImage
+                //fusion.setFusionMode(IR_MODE)
+                self.cameraView.image = irImage
                 
                 /* //test point measurement
                 if image.measurements.measurementSpots.count == 0{
@@ -183,7 +234,10 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
                                   height: boundingBox.height * cameraView.bounds.height)
             origin = CGPoint(x: boundingBox.minX * cameraView.bounds.width,
                                      y: (1 - observation.boundingBox.minY) * cameraView.bounds.height - size.height)
-            let bBox = getBoundingRectAfterRotation(rect: CGRect(origin: origin, size: size), angle: .pi/2)
+            
+            let bBox = CGRect(origin: origin, size: size)
+            
+            //let bBox = getBoundingRectAfterRotation(rect: CGRect(origin: origin, size: size), angle: .pi/2)
             
             
             let layer = CAShapeLayer()
@@ -204,8 +258,6 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
         // Do any additional setup after loading the view.
         camera.delegate = self
         discovery.delegate = self
-        readingLabel.isHidden = true
-        readingLabel.text = ""
     }
 
     
@@ -215,7 +267,37 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
         //discovery.start(FLIRCommunicationInterface.emulator)
     }
     
+    @IBAction func calibrateButtonPressed(_ sender: Any) {
+        calibrate()
+    }
+    
+    @objc private func calibrate(){
+            if camera.isConnected(){
+                camera.getRemoteControl()?.getCalibration()?.performNUC()
+                
+            }
+    }
+    
+
+    
+    /*
+    private func changeChargingLabel(state2 : FLIRChargingState){
+        guard let state2 = self.camera.getRemoteControl()?.getBattery()?.getChargingState() else {return}
+        if (state2 == FLIRChargingState(rawValue: 1)){
+            chargingLabel.text = "Not charging"
+        }else if (state2 == FLIRChargingState(rawValue: 2)){
+            chargingLabel.text = "Charging"
+        }
+    }*/
+    
+    private func getBattery(){
+        DispatchQueue.main.async {
+            Timer.scheduledTimer(withTimeInterval: 10.0, repeats: true) { timer in
+                let percent = self.battery.getPercentage()
+                self.batteryLabel.text = "\(String(percent))"
+            }
+        }
+    }
+    
     
 }
-
-
