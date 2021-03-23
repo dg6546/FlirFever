4077bbc0c1467e97713a7a2f70ef36831762a8d4
Lee Ming Tsun <dg6546@gmail.com>
2021-03-23 16:26:43 +0800

    Add support for custom coreml model

--- flirFever/ViewController.swift
+++ flirFever/ViewController.swift
@@ -16,6 +16,11 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
     var battery = FLIRBattery()
     let pm = FLIRPaletteManager()
     var flip:Int = 0
+    var image = FLIRThermalImage()
+    var faceBoxList = [FaceBox]()
+
+    
+    //var humanList = [Human]()
     
     @IBOutlet weak var chargingLabel: UILabel!
     
@@ -107,19 +112,42 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
     
     func imageReceived() {
         DispatchQueue.main.async{
-//            self.cameraView.subviews.forEach { layer in
-//                layer.removeFromSuperview()
-//            }
+            self.cameraView.layer.sublayers?.forEach { layer in
+                layer.removeFromSuperlayer()
+            }
             self.camera.withImage { (image: FLIRThermalImage) in
-                let fusion:FLIRFusion! = image.getFusion()
-                image.palette = self.pm.iron
+                self.faceBoxList = [FaceBox]()
+                self.image = image
+                let fusion:FLIRFusion! = self.image.getFusion()
+                self.image.palette = self.pm.iron
                 //image.rotate(RotationAngle(rawValue: 0)!)
                 fusion.setFusionMode(IR_MODE)
-                let irImage:UIImage! = image.getImage()
+                let irImage:UIImage! = self.image.getImage()
                 fusion.setFusionMode(VISUAL_MODE)
-                let colorImage:UIImage! = image.getImage()
+                let colorImage:UIImage! = self.image.getImage()
                 image.setTemperatureUnit(TemperatureUnit(rawValue: 0)!)
                 
+                let orientation = CGImagePropertyOrientation(rawValue: UInt32(colorImage.imageOrientation.rawValue))
+                
+                let handler = VNImageRequestHandler(cgImage: colorImage.cgImage!, orientation: orientation! )
+                do {
+                    try handler.perform([self.detectionRequest])
+                }catch{
+                    fatalError("cannot perform request")
+                }
+                self.cameraView.image = irImage
+                self.getTempDraw()
+                
+            }
+                
+                
+                
+                
+                
+                
+                
+                
+                /*
                     //fusion.setFusionMode(VISUAL_MODE)
                 
                 
@@ -159,22 +187,36 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
                         let x = bBox.midX
                         let y = bBox.midY
                         let point = CGPoint(x: x,y: y)
-                        let bBoxCenterPoint = CGPoint(x: bBox.size.width/2 + bBox.origin.x, y: bBox.origin.y) 
-                        image.measurements.addCircle(point, radius: Int32(Int(y-bBox.minY)))
+                        let bBoxCenterPoint = CGPoint(x: bBox.size.width/2 + bBox.origin.x, y: bBox.origin.y)
+                        let circlePath = UIBezierPath(arcCenter: point, radius: CGFloat(Int((y-bBox.minY)*1.2)), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true)
+                        image.measurements.addCircle(point, radius: Int32(Int((y-bBox.minY)*1.2)))
                         let label = UILabel(frame: (CGRect(origin: bBoxCenterPoint, size: CGSize(width:50,height:20))))
                         let spot  = image.measurements.measurementCircles.firstObject as? FLIRMeasurementCircle
                         guard let reading = spot?.max.value else{
                             return
                         }
                         
-                        
-                        
+                        let color:UIColor
                         //reading = reading  - 273
                         if reading > 37.5{
-                            label.textColor = UIColor.red
+                            color = UIColor.red
                         }else{
-                            label.textColor = UIColor.green
+                            color = UIColor.green
                         }
+                        let shapeLayer = CAShapeLayer()
+                        shapeLayer.path = circlePath.cgPath
+                            
+                        // Change the fill color
+                        shapeLayer.fillColor = UIColor.clear.cgColor
+                        // You can change the stroke color
+                        shapeLayer.strokeColor = color.cgColor
+                        // You can change the line width
+                        shapeLayer.lineWidth = 3.0
+                            
+                        self.cameraView.layer.addSublayer(shapeLayer)
+                        
+                        label.textColor = color
+                        
                         label.text = "\(String(format: "%.1f", (reading) ))"
                         self.cameraView.addSubview(label)
                     }
@@ -185,7 +227,7 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
                 //fusion.setFusionMode(IR_MODE)
                 self.cameraView.image = irImage
                 
-                /* //test point measurement
+                 //test point measurement
                 if image.measurements.measurementSpots.count == 0{
                     image.measurements.addSpot(CGPoint(x: Int(image.getWidth())/2, y: Int(image.getHeight())/2))
                 }
@@ -193,11 +235,133 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
                 //self.readingLabel.text = spot?.value.value() as? String
                 //print(spot?.value.value ?? "nil")
                 self.readingLabel.text = "\(String(format: "%.1f", (spot?.value.value)!-273.15+2 ))"
-                */
-            }
+                
+            }*/
             
         }
     }
+    
+    func rectInsideRect(smallRect: CGRect, bigRect: CGRect) -> Bool {
+        return (smallRect.minX >= bigRect.minX && smallRect.minY >= bigRect.minY
+                    && smallRect.maxX <= bigRect.maxX && smallRect.maxY <= bigRect.maxY)
+    }
+    
+    //called after finish detection request
+    func processDetection(for request: VNRequest, error: Error?){
+        
+        //see if there is any result
+        guard let observations = request.results as? [VNRecognizedObjectObservation] else {return}
+        
+        for observation in observations{
+                //get class label
+                let label: String? = observation.labels.first?.identifier
+                if (label == "with_mask" || label == "without_mask" || label == "mask_weared_incorrect"){
+                    if observation.confidence > 0.5{ //50% confidence threshold
+                        //add a new face box
+                        let newFaceBox = FaceBox(box: observation.boundingBox, label: label!)
+                        faceBoxList.append(newFaceBox)
+                    }
+                }else if (label == "forehead"){
+                    if observation.confidence > 0.3{ //50% confidence threshold
+                        //find which box does this forehead belongs to
+                        for facebox in faceBoxList{
+                            if ( rectInsideRect( smallRect: observation.boundingBox,bigRect: facebox.getBox())){
+                                facebox.setForeheadBox(foreheadBox: observation.boundingBox)
+                                break
+                            }
+                        }
+                    }
+                }
+            
+        }
+    }
+    
+    //get temp and draw
+    func getTempDraw(){
+        for facebox in faceBoxList{
+            let boundingBox = facebox.getBox()
+            let foreheadBox = facebox.getForeheadBox()
+            let foreheadBoxLayer = CAShapeLayer()
+            let label = facebox.getLabel()
+            var tempLabel = UILabel()
+            var color:UIColor = UIColor.green
+            
+            //create facebox layer
+            let size = CGSize(width: boundingBox.width * cameraView.bounds.width,
+                                  height: boundingBox.height * cameraView.bounds.height)
+            let origin = CGPoint(x: boundingBox.minX * cameraView.bounds.width,
+                             y: (1 - boundingBox.minY) * cameraView.bounds.height - size.height)
+            
+            let bBox = getBoundingRectAfterRotation(rect: CGRect(origin: origin, size: size), angle: .pi/2)
+            
+            let faceBoxLayer = CAShapeLayer()
+            faceBoxLayer.frame = bBox
+            faceBoxLayer.borderWidth = 2
+            
+                //create forehead box layer
+                let size2 = CGSize(width: foreheadBox.width * cameraView.bounds.width,
+                                      height: foreheadBox.height * cameraView.bounds.height)
+                let origin2 = CGPoint(x: foreheadBox.minX * cameraView.bounds.width,
+                                 y: (1 - foreheadBox.minY) * cameraView.bounds.height - size2.height)
+                
+                
+                //Dunno why but no need rotation
+                let fBox = CGRect(origin: origin2, size: size2)
+                
+                
+                foreheadBoxLayer.frame = fBox
+
+                foreheadBoxLayer.borderWidth = 2
+                
+                
+                //get forehead reading
+                
+                let x = fBox.midX
+                let y = fBox.midY
+                let point = CGPoint(x: x,y: y)
+                let radius = Int32(Int(max(fBox.midY - fBox.minY, fBox.midX - fBox.minX)*1.2))
+                //print("x:\(x)  y:\(y) radius:\(radius)")
+            
+                /* //wait until flir fix their SDK
+                image.measurements.addRectangle(foreheadBox)
+                let spot = image.measurements.measurementRectangles.firstObject as? FLIRMeasurementRectangle
+                */
+                
+                image.measurements.addCircle(point, radius: radius)
+                let spot = image.measurements.measurementCircles.firstObject as? FLIRMeasurementCircle
+                
+                guard let reading = spot?.max.value else{
+                    return
+                }
+                
+                tempLabel = UILabel(frame: (CGRect(origin: CGPoint(x: bBox.midX, y: bBox.minY), size: CGSize(width:50,height:20))))
+                
+                tempLabel.text = "\(String(format: "%.1f", (reading) ))"
+                //color
+                if fBox.midX == 0{
+                    color = UIColor.gray
+                }
+                else if reading > 37.5{
+                    color = UIColor.red
+                }else if label == "without_mask" || label == "mask_weared_incorrect"{
+                    color = UIColor.yellow
+                }
+
+            
+            foreheadBoxLayer.borderColor = color.cgColor
+            faceBoxLayer.borderColor = color.cgColor
+            tempLabel.textColor = color
+            
+            
+
+            
+            //add all sublayer
+            cameraView.layer.addSublayer(faceBoxLayer)
+            cameraView.layer.addSublayer(foreheadBoxLayer)
+            self.cameraView.addSubview(tempLabel)
+        }
+    }
+    
     func getBoundingRectAfterRotation(rect: CGRect, angle: Float) -> CGRect {
 
         let newWidth : Float = Float(rect.size.width) * abs(cosf(angle)) + Float(rect.size.height) * abs(sinf(angle))
@@ -218,6 +382,7 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
 
      }
     
+    /*
     func handleFaces(with request: VNRequest) -> [CGRect] {
         cameraView.layer.sublayers?.forEach { layer in
             layer.removeFromSuperlayer()
@@ -240,17 +405,19 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
             let bBox = getBoundingRectAfterRotation(rect: CGRect(origin: origin, size: size), angle: .pi/2)
             
             
-            let layer = CAShapeLayer()
-            layer.frame = bBox
-            layer.borderColor = UIColor.red.cgColor
-            layer.borderWidth = 2
+            //let layer = CAShapeLayer()
+            //layer.frame = bBox
+            //layer.borderColor = UIColor.red.cgColor
+            //layer.borderWidth = 2
             
-            cameraView.layer.addSublayer(layer)
+            //cameraView.layer.addSublayer(layer)
+            
+            
             bBoxList.append(bBox)
         }
         return bBoxList
     }
-                
+       */
 
     
     override func viewDidLoad() {
@@ -278,8 +445,21 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
             }
     }
     
+    lazy var detectionRequest: VNCoreMLRequest = {
+        do{
+            let model = try VNCoreMLModel(for: mask().model)
+            let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
+                self?.processDetection(for: request, error: error)
+            })
+            request.imageCropAndScaleOption = .scaleFit
+            return request
+        }catch{
+            fatalError("Cannot create model")
+        }
+    }()
 
-    
+
+
     /*
     private func changeChargingLabel(state2 : FLIRChargingState){
         guard let state2 = self.camera.getRemoteControl()?.getBattery()?.getChargingState() else {return}
@@ -301,3 +481,54 @@ class ViewController: UIViewController, FLIRDiscoveryEventDelegate, FLIRDataRece
     
     
 }
+
+class FaceBox{
+    private var box = CGRect()
+    private var label = String()
+    private var foreheadBox = CGRect()
+    
+    init(box: CGRect, label:String) {
+        self.box = box
+        self.label = label
+    }
+    
+    func setForeheadBox(foreheadBox:CGRect){
+        self.foreheadBox = foreheadBox
+    }
+    
+    func getBox() -> CGRect{
+        return self.box
+    }
+    
+    func getForeheadBox() -> CGRect{
+        return self.foreheadBox
+    }
+    
+    func getLabel() -> String{
+        return self.label
+    }
+    
+}
+
+
+/*
+class Human: Equatable{
+    var uid: UUID
+    var faceBox: CGRect
+    var foreheadBox: CGRect
+    var temp: Float
+    var tag: String
+    
+    init(uid:UUID) {
+        self.uid = uid
+        self.faceBox =  CGRect()
+        self.foreheadBox = CGRect()
+        self.temp = Float()
+        self.tag = String()
+    }
+    
+    static func ==(lhs: Human, rhs: Human) -> Bool {
+        return lhs.uid == rhs.uid
+    }
+}
+*/
